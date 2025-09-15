8598b0a2786425bddb30b6f3e3086bb0171559b7
Kabunga Charles <kcjesuschrist513@gmail.com>
2025-09-15 10:22:17 +0300

    Update calc.c

--- calc.c
+++ calc.c
@@ -43,9 +43,16 @@ printf("\n5. Modulus (%)");
             break;
 
         case '/':
-            result = num1 / num2;
-            printf("The operation %.1lf / %.1lf = %.3lf", num1, num2, result);
-            break;
+            if (num2 == 0)
+            {
+                printf("Mathematical error, Division by zero!");
+            }
+            else
+            {
+                result = num1 / num2;
+                printf("The operation %.1lf / %.1lf = %.3lf", num1, num2, result);
+            }
+        break;
         case '%':
             result = (int)num1 % (int)num2;
             printf("The operation %.1lf %% %.1lf = %.3lf", num1, num2, result);
