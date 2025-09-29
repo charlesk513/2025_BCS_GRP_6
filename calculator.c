a6ec8dbd6cfed724dc628457665decb2cc9cbe1b
Kabunga Charles <kcjesuschrist513@gmail.com>
2025-09-29 09:24:36 +0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -30,7 +30,7 @@ int modulus(int num1, int num2)
 {
     if (num2 == 0)
     {
-        printf("Error! Be serious, you can't get a modulus of %d and 0.", num1);
+        printf("Error! You can't get a modulus of %d and 0.", num1);
     }
     else
     {
